9bbfb9cc4e7f8f321677b79153d626bd50c3e7c5
mickael <mickael.bettinelli@fakeemail.com>
2017-09-26 17:38:54 +0200

    debug + main

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import argparse
+from Strategy import *
+from Agent import *
 
 import env
 
 
 def main():
-
+    strat = Strategy()
+    agent = Agent(strat)
     env1 = env.Env({'0': 1, '1': 2})
     steps = FLAGS.steps
     i = 0
 
     while i < steps:
-
-
+        action = agent.chooseExperience(i, steps)
+        result = env.getResult(action)
+        agent.get_reward(result)
         i += 1
 
 
